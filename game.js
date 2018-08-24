015462538c6d0e880a2ccbbfe0e340bb29669e30
Patryk Cholewa <patryk.cholewa@sharpeo.com>
2018-08-24 21:21:57 +0200

    Add move difference

--- game.js
+++ game.js
@@ -1,7 +1,39 @@
-injectGame();
+var player = 1;
+injectGame(3);
 
-function injectGame(){
-    document.getElementById("game").innerHTML = createGame(2);
+function injectGame( size ){
+    document.getElementById("game").innerHTML = createGame( size );
+    restart(size);
+}
+
+function restart( size ){
+    for( var i = 1 ; i <= size ; i++ ){
+        for( var j = 1 ; j <= size ; j++ ){
+            var cell = getCellIdByPosition( i , j );
+            cell.innerText = "X";
+        }
+    }
+}
+
+function handleCellClick( row , col , size ){
+    var cell = getCellByPosition( row , col );
+    var classes = cell.classList;
+    if( classes.contains("game-cell-init")){
+        classes.remove("game-cell-init");
+        if( player === 1 ){
+            classes.add("game-cell-player1");
+            cell.innerText = "X";
+            player = 2;
+        } else {
+            classes.add("game-cell-player2");
+            cell.innerText = "O";
+            player = 1;
+        }
+    }
+}
+
+function getCellByPosition( row , col ){
+    return document.getElementById( getCellIdByPosition(row,col) );
 }
 
 function createGame( size ){
@@ -14,13 +46,13 @@ function createGame( size ){
     return textHTML;
 }
 
-function createGameRow( row , colQuantity ){
+function createGameRow( row , size ){
     var textHTML = '<div id="game-row_'
         + row
         + '" class="game-row">';
 
-    for( var i = 1 ; i <= colQuantity ; i++ ) {
-        var innerHTML = createGameCell(row,i);
+    for( var i = 1 ; i <= size ; i++ ) {
+        var innerHTML = createGameCell( row , i , size );
         textHTML = textHTML + innerHTML;
     }
 
@@ -29,16 +61,24 @@ function createGameRow( row , colQuantity ){
     return textHTML;
 }
 
-function createGameCell( row , col ){
-    var textHTML = '<span id="game-cell_'
+function createGameCell( row , col , size ){
+
+    var textHTML = '<span id="'
+        + getCellIdByPosition(row,col)
+        + '" class="game-cell game-cell-init"'
+        + 'onclick="handleCellClick('
         + row
+        + ','
         + col
-        + '" class="game-cell">'
-        + '<img src="ico/one.ico"></span>';
+        + ','
+        + size
+        + ')" ></span>';
 
     return textHTML;
 
 }
 
-
+function getCellIdByPosition( row , col ){
+    return "game-cell_" + row + col;
+}
 
